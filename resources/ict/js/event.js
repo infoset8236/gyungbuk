00f2093931a88231748b736dbef07a959d2d7169
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-16 10:16:43 +0900

    [feat] c 작업중

---
+++ resources/ict/js/event.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	var mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+});
--- resources/ict/js/type_c/notice.js
+++ resources/ict/js/type_c/notice.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
-	// Slick Slider 초기화
-	$('.slider').slick({
-		infinite: true,
-		slidesToShow: 5,
-		slidesToScroll: 1, // 스크롤 수를 1로 변경
-		dots: true,
-	});
-
-	// 슬라이더에 touchmove 이벤트 리스너 추가
-	$('.slider').on('touchmove', function (event) {
-		event.preventDefault(); // 기본 동작 방지
+	var mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
 	});
 });
